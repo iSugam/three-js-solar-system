438d3acbb788bce6efd0cb5b92502adf89af769d
unknown <sugamanandsahu01@gmail.com>
2022-08-17 14:28:22 +0530

    Saving the file.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import saturnTexture from "./assets/textures/saturn.jpg";
 import saturnRingTexture from "./assets/textures/saturn_ring.png";
 import uranusTexture from "./assets/textures/uranus.jpg";
 import neptuneTexture from "./assets/textures/neptune.jpg";
+import starTexture from "./assets/textures/stars.png";
 
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
@@ -23,6 +24,12 @@ renderer.setPixelRatio(window.devicePixelRatio);
 // Add renderer to the body
 document.body.appendChild(renderer.domElement);
 
+
+// Add Scene and camera
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(100, 0, 80) // x, y, and z positions
+
 // Resize the rendering and camera as per window's width and height
 window.addEventListener("resize", () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -30,14 +37,8 @@ window.addEventListener("resize", () => {
     camera.updateProjectionMatrix()
 });
 
-
-// Add Scene and camera
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0,0.1, 25) // x, y, and z positions
-
 // Sun
-const sunGeo = new THREE.SphereGeometry( 3, 150, 150 );
+const sunGeo = new THREE.SphereGeometry( 18, 150, 150 );
 const sunText = new THREE.TextureLoader().load(sunTexture) // Load Texture image
 const sunMaterial = new THREE.MeshBasicMaterial( {map: sunText} );
 const sun = new THREE.Mesh( sunGeo, sunMaterial );
@@ -62,70 +63,79 @@ function createPlanet(size, texture, position, ring) {
         const ringMesh = new THREE.Mesh(ringGeo, ringMat);
         planetObj.add(ringMesh);
         ringMesh.position.x = position;
-        ringMesh.rotation.x =   0.5 * Math.PI
-
+        ringMesh.rotation.x = -0.6 * Math.PI
     }
     scene.add(planetObj)
     return { planetMesh, planetObj }
 }
 
-const mercury = createPlanet(0.5, mercuryTexture, 10);
-const venus = createPlanet(0.8, venusTexture, 15);
-const earth = createPlanet(1, earthTexture, 23);
-const mars = createPlanet(1.1, marsTexture, 30);
-const jupiter = createPlanet(2, jupiterTexture, 43);
-const saturn = createPlanet(1.5, saturnTexture, 53, {
-    innerRadius: 2,
-    outerRadius: 4,
-    texture: saturnRingTexture
+const mercury = createPlanet(0.8, mercuryTexture, 30);
+const venus = createPlanet(1, venusTexture, 40);
+const earth = createPlanet(1.6, earthTexture, 55);
+const mars = createPlanet(1.5, marsTexture, 63);
+const jupiter = createPlanet(2.8, jupiterTexture, 73);
+const saturn = createPlanet(2.4, saturnTexture, 85, {
+    innerRadius: 2.5,
+    outerRadius: 5,
+    texture: saturnRingTexture,
 });
-const uranus = createPlanet(1.2, uranusTexture, 66);
-const neptune = createPlanet(1.1, neptuneTexture, 75);
-
-// saturn.planetObj.add(saturnRing)
-
+const saturnRing = saturn.planetObj.children[1];
+const uranus = createPlanet(1.4, uranusTexture, 98);
+const neptune = createPlanet(1.3, neptuneTexture, 110);
 
 const pointLight = new THREE.PointLight( 0xffffff, 3, 300)
 scene.add( pointLight );
 
 // Add orbit controls to camera
 const controls = new OrbitControls( camera, renderer.domElement );
+controls.panSpeed = 0.04
 controls.update();
 
 
+// STARS Particles
 const vertices = [];
 
 for ( let i = 0; i < 10000; i ++ ) {
-	const x = THREE.MathUtils.randFloatSpread( 2000 );
+	const x = THREE.MathUtils.randFloatSpread( 8000 );
 	const y = THREE.MathUtils.randFloatSpread( 2000 );
 	const z = THREE.MathUtils.randFloatSpread( 2000 );
 	vertices.push( x, y, z );
 }
 
-const geometry = new THREE.BufferGeometry();
-geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-
-const material2 = new THREE.PointsMaterial( { color: 0x00ddff } );
-const points = new THREE.Points( geometry, material2 );
-
-scene.add( points );
+const StarGeometry = new THREE.BufferGeometry();
+StarGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+const starMaterial = new THREE.PointsMaterial( { 
+    map: new THREE.TextureLoader().load(starTexture), 
+    size: 3, 
+    transparent: true, 
+    depthTest: false
+});
+const stars = new THREE.Points( StarGeometry, starMaterial );
+scene.add( stars );
 // Recursive function to keep re-rendering
 function animate() {
     requestAnimationFrame(animate);
 
-    // Rotaions
+    // Spinning
     mercury.planetMesh.rotateY(0.008);
-    mercury.planetObj.rotateY(0.005);
     venus.planetMesh.rotateY(0.0001);
-    venus.planetObj.rotateY(0.003);
     earth.planetMesh.rotateY(0.05);
+    mars.planetMesh.rotateY(0.05);
+    jupiter.planetMesh.rotateY(0.1);
+    saturn.planetMesh.rotateY(0.1);
+    saturnRing.rotateZ(0.05)
+    uranus.planetMesh.rotateY(0.09);
+    neptune.planetMesh.rotateY(0.09);
+
+    // Rotation around the Sun
+    mercury.planetObj.rotateY(0.005);
+    venus.planetObj.rotateY(0.003);
     earth.planetObj.rotateY(0.001);
-
-    mars.planetMesh.rotateY(0.03)
-    mars.planetObj.rotateY(0.003);
-
-    jupiter.planetMesh.rotateY(0.1)
-    jupiter.planetObj.rotateY(0.0002);
+    mars.planetObj.rotateY(0.0009);
+    jupiter.planetObj.rotateY(0.0004);
+    saturn.planetObj.rotateY(0.00035);
+    uranus.planetObj.rotateY(0.00025);
+    neptune.planetObj.rotateY(0.00018);
 
     sun.rotation.y += 0.001
     controls.update();
