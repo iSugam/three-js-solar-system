6dd63ed41003de93d6f7c77f15e2d46327287636
unknown <sugamanandsahu01@gmail.com>
2022-09-02 18:15:26 +0530

    Exporting three js from Three.js to use in as a common component.

---
+++ src/app/Three.js
@@ -0,0 +1,7 @@
+import * as THREE from "three";
+import { DoubleSide } from "three";
+
+// Eporting all to use as common file.
+export const scene = new THREE.Scene();
+export const doubleSide = DoubleSide;
+export default THREE;
